0bd46c65859bb99e4610732244776b239e58880d
alin Mican <alinmican2001@gmail.com>
2023-12-12 20:55:55 +0000

    Works on easier display for user.

--- hang.py
+++ hang.py
@@ -1,37 +1,32 @@
 class Hangman:
     lifes = 7
-    def __init__(self,word):
+    correct_letter = 0
+
+    def __init__(self, word):
         self.word = word
-        self.guessed = ["_"] * len(self.word)
-        
-    #Return True or False if the letter guessed is correct.
-    def guess_word(self,guess):
+        self.guessed = ["_"] * (len(self.word) - 1)
+
+    # Return True or False if the letter guessed is correct.
+    def guess_word(self, guess):
         if guess not in self.word:
             return False
         return True
-    #Reveal letter based on guess
-    
-    
-    
-    def reveal_letter(self,letter):
+
+    # Reveal letter based on guess
+    def reveal_letter(self, letter):
         for i in range(len(self.word)):
-            
             if self.word[i] == letter:
-                
+                correct_letter += 1
                 self.guessed[i] = letter
-        return self.guessed
-    
-    def wrong_guess(self,lifes):
-        
-        array = ['_','_','_','_','_','_','_']
-        string = 'WRONG!!'
+        return " ".join(self.guessed)
+
+    def wrong_guess(self, lifes):
+        array = ["_", "_", "_", "_", "_", "_", "_"]
+        string = "WRONG!!"
         string = string[::-1]
-        array[lifes] = string[lifes]
-        lifes-=1
-        return array[::-1]
+        array[lifes] += string[lifes]
+
+        return " ".join(array[::-1])
 
     def hide_word(self):
         return self.guessed
-        
-    
-    
--- main.py
+++ main.py
@@ -1,14 +1,37 @@
+import random
+
 from hang import Hangman
-word = "abcdeb"
+
+# Open the file and read the words from it
+
+with open("hangman_words.txt", "r") as file:
+    words = file.readlines()
+
+file.close()
+
+# Store all words in an array
+word_list = []
+for word in words:
+    word_list.append(word)
+random_word = random.randint(0, (len(words) - 1))
+
+
+print(word_list[random_word])
+# Put the random word into hangman guessing
+word = word_list[random_word]
 hangman = Hangman(word)
 hangman.lifes -= 1
+#Print the blank of how many words the user needs to guess.
+print(f"{" ".join(hangman.hide_word())}\n")
 
-print(hangman.hide_word())
 while hangman.lifes >= 0:
-    letter_guess = input("Enter: ")
+    letter_guess = input("Enter a letter to guess: ")
     if hangman.guess_word(letter_guess):
-        print(hangman.reveal_letter(letter_guess))
-    else:
         
+        print(hangman.reveal_letter(letter_guess))
+        hangman.correct_guess += 1
+        print(hangman.correct_guess)
+    else:
         print(hangman.wrong_guess(hangman.lifes))
         hangman.lifes -= 1
+
